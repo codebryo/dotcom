3ec14a6cd0aaf2b1e64fbecb5cb70426f6b0668a
Roman Kuba <roman.kuba@gmail.com>
2019-08-29 20:09:35 +0200

    added tags component

---
+++ components/tags.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  functional: true,
+
+  render(h, context) {
+    const tags = context.props.tags
+    const tagClasses =
+      'inline-block p-1 border rounded border-gray-700 border-solid mr-1'
+
+    return [
+      ...tags.map(tag =>
+        h(
+          'span',
+          {
+            class: tagClasses.split(' '),
+            key: tag
+          },
+          tag
+        )
+      )
+    ]
+  }
+}
+</script>
